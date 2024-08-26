8857db0e77207693050aa45b3f5e90bb3c8f653d
Justin Smith <justsmth@amazon.com>
2024-08-26 09:26:16 -0400

    Clarify loop conditions in NTT

--- crypto/kyber/pqcrystals_kyber_ref_common/ntt.c
+++ crypto/kyber/pqcrystals_kyber_ref_common/ntt.c
@@ -79,14 +79,13 @@ static int16_t fqmul(int16_t a, int16_t b) {
 **************************************************/
 void ntt(int16_t r[256]) {
   unsigned int len, start, j, k;
-  int16_t t, zeta;
 
   k = 1;
   for(len = 128; len >= 2; len >>= 1) {
-    for(start = 0; start < 256; start = j + len) {
-      zeta = zetas[k++];
+    for(start = 0; start < 254; start = j + len) {
+      const int16_t zeta = zetas[k++];
       for(j = start; j < start + len; j++) {
-        t = fqmul(zeta, r[j + len]);
+        const int16_t t = fqmul(zeta, r[j + len]);
         r[j + len] = r[j] - t;
         r[j] = r[j] + t;
       }
